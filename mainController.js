3506c30d3c5fb7b5232cde39fc1736bd1c49928f
Chiedo <chiedo@chie.do>
2016-06-08 11:16:11 -0400

    Keys in memory encrypted and removed xss possibility

--- mainController.js
+++ mainController.js
@@ -1,19 +1,35 @@
 'use strict';
-let path = require('path');
+let path   = require('path');
+let crypto = require('crypto');
 
 let passwords = {};
+let key = String(Math.random()); // used for encryption and decryption
+
+function encrypt(text){
+  let cipher = crypto.createCipher('aes-256-cbc',key);
+  let crypted = cipher.update(text,'utf8','hex');
+  crypted += cipher.final('hex');
+  return crypted;
+}
+
+function decrypt(text){
+  let decipher = crypto.createDecipher('aes-256-cbc',key);
+  let dec = decipher.update(text,'hex','utf8');
+  dec += decipher.final('utf8');
+  return dec;
+}
 
 module.exports.index = (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
 };
 
 module.exports.create = (req, res) => {
-  return require('crypto').randomBytes(48, (err, buffer) => {
+  return crypto.randomBytes(48, (err, buffer) => {
     let token = buffer.toString('hex');
-    passwords[token] = req.body.message;
+    passwords[token] = encrypt(req.body.message);
 
     let protocol;
-    if(process.env.NODE_ENV='production') {
+    if(process.env.NODE_ENV === 'production') {
       protocol = 'https';
     } else {
       protocol = 'http';
@@ -24,8 +40,16 @@ module.exports.create = (req, res) => {
 };
 
 module.exports.show = (req, res) => {
+  // Prevent script injection
+  res.setHeader('content-type', 'text/plain');
+
   let key = req.originalUrl.replace('/','');
-  let x = passwords[key];
-  passwords[key] = undefined;
-  return res.send(`<pre>${x}</pre>`);
+  let x;
+
+  if(passwords[key]) {
+    x = decrypt(passwords[key]);
+    passwords[key] = undefined;
+  }
+
+  return res.send(`${x}`);
 };
